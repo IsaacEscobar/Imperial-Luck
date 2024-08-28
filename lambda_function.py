92fb2d4ecea31966b0996e09d5cadfc96c71e412
IsaacEscobar <isak.escobarr@gmail.com>
2024-08-28 10:35:54 -0500

    rollback policy response

--- lambda_function.py
+++ lambda_function.py
@@ -8,7 +8,7 @@ def lambda_handler(event, context):
     You have the right to access, correct, and delete your data at any time. For more details, please review our Privacy Policy.
     """
     
-    response = "yes" #event['response']
+    response = event['response']
 
     if response.lower() == "yes":
         statuscode = 200
